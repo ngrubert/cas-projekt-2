3678d718ff3557f5da323efb91095672e86ecd63
ngrubert <nicogrubert@gmail.com>
2017-02-06 18:10:04 +0100

    manage route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
- { path: '', redirectTo: '/index', pathMatch: 'full' },
- { path: 'home', loadChildren: './+home/home.module#HomeModule' },
- { path: 'create', loadChildren: './+create/create.module#CreateModule' },
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'home', loadChildren: './+home/home.module#HomeModule' },
+  { path: 'create', loadChildren: './+create/create.module#CreateModule' },
   { path: 'help', loadChildren: './+help/help.module#HelpModule' },
   { path: 'list/:id', loadChildren: './+list/list.module#ListModule' },
-  { path: 'manage', loadChildren: './+manage/...' },
+  { path: 'manage', loadChildren: './+manage/manage.module#ManageModule' },
   { path: 'lists', loadChildren: './+lists/lists.module#ListsModule' },
   { path: 'editlist', loadChildren: './+edit/edit.module#EditModule' },
   { path: 'clearlist', loadChildren: './+clear/clear.module#ClearModule' },
