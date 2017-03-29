030ab5f6ea08212ab6839ee5961636c4def59b53
Andreas Karrer <andreas.karrer@hsr.ch>
2017-03-29 13:17:32 +0200

    modularize mail account

--- firebase-secret.js
+++ firebase-secret.js
@@ -2,13 +2,13 @@
 var firebase = {
     "type": "service_account",
     "project_id": "fergg-c183c",
-    "private_key_id": "9408278cba93626da978b57d8a175913453063b3",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC3+aXJ/6e4LQg+\n1LU0Lkgz8VDgJ4DcAgiXiSjQQxT3UJlUfEuep56n7Gsu3WHcxLj+YbPJ0u/5mZWE\n1y8uJGh4/dnIzocwRfJBthEtRip16Kd2yUInmpBjdZ9UMebrqmrF+KsECjU9q8q1\nJ3iAS/EdrCETwo8SI8GHDaaPG84r2CMnADLoN6rsKKO+SEhMBZUW6EivJVF/WJ6Y\nh0fYIh0gOy5HTZ4dVrIC4q1nv4cagiRBhAp91kDvgR+PdYice8Nu5hNWMBb0742S\ntzePSQdgN4zjnp+XuiDxIzHZAexXZ0aEQy+JU2MC1pVNxu9FOSKhXWKwPC/sH7MV\nFXokkJ65AgMBAAECggEBAKEzTeFA8WSfkKn9FwkxxHI+7+54rqAX6A9fTzOsk6Jg\nLPmqL1hE5u+caA4YTLd0Z+P9F9uvyDWcLTbIlb6IiEH7q+gXjrztjOem8EYLRQkr\nYhDjJ+Qrunhrh7/uObglComM6cwQZbYz4Nn6S8uevxaQzCrUPyhVzCzZ+SdL5crj\niq4QWPegVGLHQ6K91gF9er28S8+sjMJVlLl9f7III75K1RyQLP1aBRe5jvZeyBY9\nPUo1OsK/MQ6ELCHhuKgwLqyDBJ6NDH6EPwBGpz3b1ColNdfOs8JyNHhH5Vf0nZ8A\nYHnnFpLcKVIQEh4kdg/ZhHtvV7qVGkl+OdqxaurhoAECgYEA427P9TFJ1N6/F4qe\nT6yUmnGdXlG70NU1cM1FwuEbWn/YbTJwKb1uWMpFkVEhVmu4JpDGWH6furFKQzST\nbeJQfeiewqFzGoOyOopH4yqO4SLLDKDW0n6p61lXOhGgOH1GZ9ugJx5kInPNx3hr\nF5lypZT8gvm/xg4/4UTtj4j2uPkCgYEAzxVwDJrIsQTNBep5SSgFXD7C63PqMjUl\ntxtG+IUmedeZzhoMavhgvbdM1a/XwnT9HkErziLni9Eg/JGaKGDTXRPuOGA0ssd5\nG/JedmW2SeC5wB0ZNRcuotKESvL9ml8YPcQQl5okaMvWEggv4pCsE9DfeAcH1xPn\nXZQL7jDWQ8ECgYA2Cv3lgKnPqIJ2AJ1vx02kiXzgjcF4dUF2pYB/N2h9j8WI5SRb\n9sSC2sW/Ky0l0QD4QQywuJ4SCdDguVyTDuUbliwriZvFYCf42yUL6O/8tXzUal/B\nsxGDeD5O8SNGaRZS1ZBv2zvmJ+htDsM7312r1QRPxzL3yCkLyYo9wkkGWQKBgFmx\nrWCoQjj6HVtsna/kF75GqOKWyF+KyZUvyWOnOG0Zx1KP0cUbY7uIg00i5s8hIOlT\nzQdx8tyKbkE3R5ApsCLmmQ3uDaVJ5NkH94ZMs9H39jyR3fMy4U5A5HAaI2ITwkou\nfAlOCGsjrssMpMD7bK0AkWpJkUnFIzdTAn/ew0+BAoGBAICpR9bpKsf5EjP9+Od7\n4Cs09g7XgR2FWvoL1NFb5PJ575Wc9i5sYkrCOR5T66BqRtF+TuP64XKIeRygqmI5\nkhOLXvd43Gyfve4QMzfFJFakgj3tG13jFI0n2rjNCjQJDrwYRpjqw8MQPm66zl+7\n8nFRrrPavBxph+hz4DaFu+ht\n-----END PRIVATE KEY-----\n",
+    "private_key_id": "-PRIVATE-KEY-ID-HERE-",
+    "private_key": "-PRIVATE-KEY-HERE-",
     "client_email": "firebase-adminsdk-w9uj0@fergg-c183c.iam.gserviceaccount.com",
     "client_id": "110027358273547978551",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://accounts.google.com/o/oauth2/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-w9uj0%40fergg-c183c.iam.gserviceaccount.com"
-}
+};
 module.exports = firebase;
---
+++ mail-secret.js
@@ -0,0 +1,7 @@
+var mailAccount = {
+    "smtpHost": "saphir.metanet.ch",
+    "smtpUser": "fergg@karrer.net",
+    "fromUser": "fergg@karrer.net",
+    "smtpPass": "-PASSWORD-HERE-"
+};
+module.exports = mailAccount;
